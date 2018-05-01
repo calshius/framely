5cae3f2abb8a9a0b8668a6589d0be743c99a506c
calshius <callumfay_16@hotmail.co.uk>
2018-05-01 20:19:52 +0200

    Added new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Footer from './Footer'
-import avatar from '../assets/images/framely.jpg'
+import avatar from '../assets/images/logo.png'
 
 class Header extends React.Component {
     render() {
